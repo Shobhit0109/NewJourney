a21b33dc9fb84e7aabcc7b074463ecfebbf177f3
Shobhit0109 <shobhit.ug21@nsut.ac.in>
2023-01-03 15:12:09 +0530

    Tuesday 03 January 2023 03:12:09 PM IST

--- New Programs/All C/C++/practice.cpp
+++ New Programs/All C/C++/practice.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <pthread.h>
 using namespace std;
 
 #define f(i, a, n) for (int i = a; i < n; i++)
+#define num 100
+#define int long long
 
-int main(void)
+int fibonacci(int *arr,int n)
 {
-    cout << sizeof(long long int*) << endl;
-    int a;
-    cin >> a;
+    // pthread_t th;
+    if (n < 2)
+        return n;
+    if (arr[n-1] != 0)
+        return arr[n-1];
+    arr[n-1] = fibonacci(arr,n - 1) + fibonacci(arr,n - 2);
+    return arr[n-1];
+}
+
+signed main(void)
+{
+    int *arr = new int[num];
+    cout << fibonacci(arr,num)   << endl;
+    delete[] arr;
     return 0;
 }
